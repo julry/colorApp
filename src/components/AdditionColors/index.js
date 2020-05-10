198f9bc05f0eb07f98644b962ec04dc8f9709115
Дмитрий Маньшин <manshin.dmitry@gmail.com>
2020-05-11 00:25:07 +0300

    Add dnd logic

--- src/components/AdditionColors/index.js
+++ src/components/AdditionColors/index.js
@@ -1,25 +1,20 @@
 import React from "react";
 import "./index.css";
 import AdditionBlock from "../AdditionBlock";
-import { DndProvider } from 'react-dnd';
-import Backend from 'react-dnd-html5-backend';
 
-const AdditionColors = ({colors, isActive, amount }) => {
-  if (!isActive) return <div></div>;
-  return(
-    <DndProvider backend={Backend}>
-  <div className="addition-colors">
-    {[...new Array(amount)].map((item, index) => (
-    <AdditionBlock
-      color = {colors[index]}
-      id = {index}
-      key = {index}
-     />))
-    }
-  </div>
-</DndProvider>
-)
-    
+const AdditionColors = ({colors, isActive, amount}) => {
+  if (!isActive) return null;
+  return (
+    <div className="addition-colors">
+      {[...new Array(amount)].map((item, index) => (
+        <AdditionBlock
+          color={colors[index]}
+          index={index}
+          key={index}
+        />))
+      }
+    </div>
+  )
 };
 
-  export default AdditionColors;
+export default AdditionColors;
--- src/components/Pyramid/index.js
+++ src/components/Pyramid/index.js
@@ -1,33 +1,27 @@
 import React from "react";
 import './index.css';
 import ColorBlock from "../ColorBlock";
-import { DndProvider } from 'react-dnd';
-import Backend from 'react-dnd-html5-backend';
 
 const Pyramid = (props) => {
- 
-  console.log(props.colors);
-    return (
-    <DndProvider backend={Backend}>
-      <div className="pyramid">
-          {[...new Array(props.level)].map((item, rowIndex) => (
-            <div className="row" key={rowIndex}>
-                {[...new Array(rowIndex + 1)].map((row, index) => (
-                  <ColorBlock 
-                  correct = {props.correctColors}
-                  color = {props.colors[props.convert(rowIndex, index)]}
-                  key={index} 
-                  rowIndex={rowIndex} 
-                  index = {index} 
-                  convertedIndex = {props.convert(rowIndex, index)}
-                  >
-                  </ColorBlock>
-                ))}
-            </div>
+  return (
+    <div className="pyramid">
+      {[...new Array(props.level)].map((item, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {[...new Array(rowIndex + 1)].map((row, index) => (
+            <ColorBlock
+              correct={props.correctColors[props.convert(rowIndex, index)]}
+              color={props.colors[props.convert(rowIndex, index)]}
+              key={index}
+              rowIndex={rowIndex}
+              index={index}
+              convertedIndex={props.convert(rowIndex, index)}
+              onGuess={props.onColorGuess}
+            />
           ))}
-      </div>
-      </DndProvider>
-    );
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Pyramid;
